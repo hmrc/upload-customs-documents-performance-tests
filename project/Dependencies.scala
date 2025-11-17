a0872a06f669a510c6d424c8418726c9c102ba4a
abutters <23127331+abutters@users.noreply.github.com>
2025-11-17 15:49:37 +0000

    DCWL-4081 Addressed pr-bot comments.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,13 +4,11 @@ object Dependencies {
 
   private val gatlingVersion = "3.14.3"
 
-  val hmrcMongoPlayVersion = "2.7.0"
+  val hmrcMongoPlayVersion = "2.10.0"
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.4.4"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0"        % Test,
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0"        % Test,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion % Test
   )
 }
